dcdc5d6d2b548d553c339235e03dc284e30b8101
zgl <3612651205@qq.com>
2022-11-08 23:20:40 +0800

    两数之和

--- src/main/java/com/example/tree/array/TwoSum.java
+++ src/main/java/com/example/tree/array/TwoSum.java
@@ -1,36 +1,23 @@
 package com.example.tree.array;
 
-/**
- *  两数之和 II - 输入有序数组
- *  方法 搞两个指针 ，一个在头一个尾巴，相加比较一下是否小于目标值，
- *  如果小于那么就挪动 头指针，然后再加，如果大于就挪动尾指针，继续循环，最终就能得到两个小标
- */
+import java.util.HashMap;
+import java.util.Map;
+
 public class TwoSum {
-
-    public static int[] twoSum(int[] numbers, int target) {
-        int i=0;
-        int j = numbers.length - 1;
-        while (i < j) {
-            if (numbers[i] + numbers[j] < target) {
-                i++;
-            } else if (numbers[i] + numbers[j] > target) {
-                j--;
-            } else {
-                break;
-            }
-        }
-        return new int[]{i + 1, j + 1};
-    }
-
-
     /**
-     * [2,7,11,15]
-     * @param args
+     * 思路 搞一个map 存key 是值，value是下标，每次遍历的时候把这个东西存起来。然后找目标数-当前数的值。是否能在map扎到。 找打了就返回两个下标
+     * @param nums
+     * @param target
+     * @return
      */
-    public static void main(String[] args) {
-        int[] ints = {2, 7, 11, 15};
-        int[] ints1 = twoSum(ints, 9);
-        System.out.println(ints1[0]);
-        System.out.println(ints1[1]);
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> integerMap = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (integerMap.containsKey(target - nums[i])) {
+                return new int[]{integerMap.get(target - nums[i]), i};
+            }
+            integerMap.put(nums[i], i);
+        }
+        return new int[0];
     }
 }
